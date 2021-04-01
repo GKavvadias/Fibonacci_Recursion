f3f31b4f9afc51076ba104f3b035080eca0f11e8
unknown <80004643+GKavvadias@users.noreply.github.com>
2021-04-01 17:19:44 +0100

    Initial commit

---
+++ Fibonacci_Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Παρακαλώ δώστε έναν αριθμό: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Ο αριθμός Fibonacci είναι το νούμερο: {Fibonacci(n)}");
+            Console.ReadKey();
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
